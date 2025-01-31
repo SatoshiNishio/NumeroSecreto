1948a5e9abfb30afd5763e8da75ceecda2fcade9
SatoshiNishio <mayconsatoshifn@gmail.com>
2025-01-31 19:11:04 +0000

    Updated error

--- logica-js-aula_1/app.js
+++ logica-js-aula_1/app.js
@@ -1,11 +1,15 @@
-alert("WelcomeToThisUselessGame!");
-let numeroSecreto = 25; 
-console.log(numeroSecreto);
-let chute prompt(choose your destiny 1-100);
+alert('WelcomeToThisUselessGame!');
+let numeroSecreto = 5; 
+console.log(numeroSecreto)
+let chute = prompt("choose your destiny 1-100");
 
-// se o chute for igual ao numero secreto;
-if(chute == numeroSecreto);{
-    alert(`Congratulations! The number is ${numeroSecreto} , You win!`);
-}else {
-alert (you lose) :("")
+// se o chute for igual ao numero secreto
+if (chute == numeroSecreto) {
+    alert(`Congratulations! The number is ${numeroSecreto}, You win!`);
+} else {
+    if (chute > numeroSecreto) {
+        alert(`The number is less than ${chute}`);
+    } else {
+        alert(`The number is more than ${chute}`);
+    }
 }
--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,17 +1,21 @@
 alert('WelcomeToThisUselessGame!');
-let numeroSecreto = 25; 
-console.log(numeroSecreto)
+let numeroSecreto = 5; 
+console.log(numeroSecreto);
 let chute = prompt("choose your destiny 1-10");
+let tentativas = 1;
 
 //enquanto chute nao for igual a n.s. 
 while (chute != numeroSecreto) 
     // se o chute for igual ao numero secreto
-if(chute == numeroSecreto) {
-    alert(`Congratulations! The number is ${numeroSecreto} , You win!`);
-} else {
+if (chute == numeroSecreto) {
+    alert(`Congratulations! The number is ${numeroSecreto} with ${tentativas}, You win!`);}
+  else {
     if (chute > numeroSecreto) {
         alert(`The number is more than ${chute}`);
-    } else {
+    }else {
+        if (chute < numeroSecreto)
         alert(`The number is less than ${chute}`);
     }
-}
+    //tentativas = tentativas + 1;
+    tentativas++;
+} 
